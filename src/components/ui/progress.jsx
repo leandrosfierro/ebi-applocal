78c2a2b9d653ee00f3964e329c580327e49eb94c
leandrosfierro <leandrosfierro@gmail.com>
2025-08-25 17:33:21 -0300

    Delete src directory

--- src/components/ui/progress.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Progress({ value = 0 }) {
-  return (
-    <div className="w-full h-2 bg-gray-200 rounded">
-      <div className="h-2 bg-slate-900 rounded" style={{ width: `${value}%` }} />
-    </div>
-  );
-}
