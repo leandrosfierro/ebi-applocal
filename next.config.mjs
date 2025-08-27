f4f570488cc226d5019d183716ba8d73aeba67bc
leandrosfierro <leandrosfierro@gmail.com>
2025-08-27 15:55:56 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true },
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'placehold.co' }] }
-}
-export default nextConfig
